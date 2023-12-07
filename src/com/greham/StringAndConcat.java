93c6da2cb9ddd1ca9af41003ff8db8807230a3c8
Aridunnu <aridunnuade@gmail.com>
2023-12-07 23:08:12 +0100

    update

---
+++ src/com/greham/StringAndConcat.java
@@ -0,0 +1,10 @@
+package com.greham;
+
+public class StringAndConcat {
+    public static void main(String[] args) {
+        int age = 35;
+        String description = " , This is the also known as thirty-fine." ;
+        System.out.println(age + description);
+
+    }
+}
